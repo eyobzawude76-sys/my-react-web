2484525a534e96401585329bd36c273b643db16a
EYOB <eyobzawude76@gmail.com>
2026-03-19 00:24:36 +0300

    cheng

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,18 @@ return(
 
 <Router>
    
-<div className='App'>
-<Navbar  bg="dark" variante="dark" expand="lg" fixed='top' className='shadow-sm'>
+<div className='App'> 
+<Navbar  bg="danger" variante="danger" expand="lg" fixed='top' className='shadow-sm'>
   <Container>
     <Navbar.Brand   as={Link} href="/" className='fw-bold fs-4' style={{color:"white"}}> Eyob's Website   </Navbar.Brand>
     
-    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" className='border-0'/>
     
-<Navbar.Collapse id="basic-navbar-nav">
+<Navbar.Collapse id="basic-navbar-nav"  > 
 <Nav className="ms-auto">
-<Nav.Link as={Link} to="/" className='nav-Link-custom' style={{color:"white"}}>Home</Nav.Link>
-<Nav.Link as={Link} to="/About" className='nav-Link-custom' style={{color:'white'}}>About</Nav.Link>
-<Nav.Link as={Link} to="/Contant" className='nav-Link-custom' style={{color:"white"}}>Contact</Nav.Link>
+<Nav.Link as={Link} to="/" className='nav-Link-custom px-3' style={{color:"white"}}>Home</Nav.Link>
+<Nav.Link as={Link} to="/About" className='nav-Link-custom px-3' style={{color:'white'}}>About</Nav.Link>
+<Nav.Link as={Link} to="/Contant" className='nav-Link-custom px-3' style={{color:"white"}}>Contact</Nav.Link>
 
 </Nav>
 </Navbar.Collapse></Container></Navbar>
